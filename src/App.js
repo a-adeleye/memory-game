e1fdbb9699f47522a68f34b6a7cfadeffea69a3d
aadeleye <adeleyeadeyemib@gmail.com>
2022-02-07 18:19:16 +0100

    header css completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import goddessLogo from "./goddess-logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+          <img className="logo--image" alt="logo-image" src={goddessLogo} />
+          <h1 className="logo--title">Goddess Memory Game</h1>
+        </div>
+        <div className="score-board">
+          <h2 className="score-board--score">Score: 10</h2>
+          <div className="score-board--separator"></div>
+          <h2 className="score-board--best">Best: 20</h2>
+        </div>
       </header>
     </div>
   );
