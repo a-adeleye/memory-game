ba6753df131e875c1bb125766ed17e3727228366
aadeleye <adeleyeadeyemib@gmail.com>
2022-02-07 18:58:45 +0100

    gameboard completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,102 @@
 import goddessLogo from "./goddess-logo.jpg";
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img className="logo--image" alt="logo" src={goddessLogo} />
+        <h1 className="logo--title">Goddess Memory Game</h1>
+      </div>
+      <div className="score-board">
+        <h2 className="score-board--score">Score: 10</h2>
+        <div className="score-board--separator"></div>
+        <h2 className="score-board--best">Best: 20</h2>
+      </div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section className="gameboard">
+      <h1 className="level">Level 1</h1>
+
+      <div className="cards">
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+        <div className="card">
+          <img src="" alt="name"/>
+          <h4 className="card-description">Yemoja</h4>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="logo">
-          <img className="logo--image" alt="logo-image" src={goddessLogo} />
-          <h1 className="logo--title">Goddess Memory Game</h1>
-        </div>
-        <div className="score-board">
-          <h2 className="score-board--score">Score: 10</h2>
-          <div className="score-board--separator"></div>
-          <h2 className="score-board--best">Best: 20</h2>
-        </div>
-      </header>
+      <Header></Header>
+      <Main></Main>
     </div>
   );
 }
